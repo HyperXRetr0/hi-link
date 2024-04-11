10557a50d0ecbad0b308025792e6da60632a07e1
Mayank Gupta <gupta.mayank2003@gmail.com>
2024-04-11 22:52:12 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "@/components/Hero";
+import GetApp from "@/components/GetApp";
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import Features from "@/components/Features";
+
+export default function Home() {
+  return (
+      <>
+        <Hero />
+        <Camp />
+        <Guide />
+        <Features />
+        <GetApp />
+      </>
+  );
+}
